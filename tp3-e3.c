bd867db4257fe969eafc732dfc90d3f532e743de
Mikael <lescanostisman.mikael@gmail.com>
2024-04-16 13:44:43 -0300

    punto 3 completado

--- tp3-e3.c
+++ tp3-e3.c
@@ -2,54 +2,29 @@
 #include <string.h>
 #include <stdio.h>
 
+#define MAX_CARACT 100
+#define CANT_NOMBRES 5
+
 int main()
 {
-    char Buffer[100];
-    char *nombre1, *nombre2, *nombre3, *nombre4, *nombre5;
+    char Buffer[MAX_CARACT], *Nombres[CANT_NOMBRES];
     int tamano;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < CANT_NOMBRES; i++)
     {
         printf("Escriba un nombre: ");
         gets(Buffer);
-
         tamano = strlen(Buffer) + 1;
-        switch (i)
-        {
-            case 0:
-                nombre1 = (char *) malloc(tamano * sizeof(char));
-                strcpy(nombre1, Buffer);
-                break;
-            case 1:
-                nombre2 = (char *) malloc(tamano * sizeof(char));
-                strcpy(nombre2, Buffer);
-                break;
-            case 2:
-                nombre3 = (char *) malloc(tamano * sizeof(char));
-                strcpy(nombre3, Buffer);
-                break;
-            case 3:
-                nombre4 = (char *) malloc(tamano * sizeof(char));
-                strcpy(nombre4, Buffer);
-                break;
-            case 4:
-                nombre5 = (char *) malloc(tamano * sizeof(char));
-                strcpy(nombre5, Buffer);
-        }
+        Nombres[i] = (char *) malloc(tamano * sizeof(char));
+        strcpy(Nombres[i], Buffer);
     }
 
     printf("\nNOMBRES:\n");
-    puts(nombre1);
-    puts(nombre2);
-    puts(nombre3);
-    puts(nombre4);
-    puts(nombre5);
-
-    free(nombre1);
-    free(nombre2);
-    free(nombre3);
-    free(nombre4);
-    free(nombre5);
+    for (int i = 0; i < CANT_NOMBRES; i++)
+    {
+        puts(Nombres[i]);
+        free(Nombres[i]);
+    }
 
     return 0;
 }
